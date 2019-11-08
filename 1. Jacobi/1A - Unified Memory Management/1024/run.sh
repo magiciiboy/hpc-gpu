73fc43d2a50a5905d3161a4716f92053fbe0d9db
Tung <tung.huynh@seedcom.vn>
2019-11-08 17:59:22 -0600

    Update report script

--- 1. Jacobi/1A - Unified Memory Management/1024/run.sh
+++ 1. Jacobi/1A - Unified Memory Management/1024/run.sh
@@ -1,6 +1,16 @@
 pgcc -acc -fast -ta=tesla:cc60,managed -Minfo=accel jacobi.c laplace2d.c -o jacobi
 
-nvprof --cpu-profiling on --metrics dram_read_transactions ./jacobi > report_DRAM.log
-nvprof --cpu-profiling on --metrics flop_count_dp ./jacobi > report_DP.log
-nvprof --system-profiling on ./jacobi > report_Time_Power.log
+# DRAM
+rm report_DRAM.log
+nvprof --cpu-profiling on --metrics dram_read_transactions ./jacobi >> report_DRAM.log
+
+# DP
+rm report_DP.log
+nvprof --cpu-profiling on --metrics flop_count_dp ./jacobi >> report_DP.log
+
+# Time vs Power
+rm report_Time_Power.log
+nvprof --system-profiling on ./jacobi >> report_Time_Power.log
+
+# Human-readable report
 nvprof --csv --system-profiling on --devices 0 --log-file report.log ./jacobi
