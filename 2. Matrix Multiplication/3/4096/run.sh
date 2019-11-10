88a5f2ce02f0edd5f6d4ac273fc190f49e407746
Tung <tung.huynh@seedcom.vn>
2019-11-09 18:26:43 -0600

    Add matmul script

---
+++ 2. Matrix Multiplication/3/4096/run.sh
@@ -0,0 +1,37 @@
+module load cuda
+module load pgi
+pgcc -acc -fast -ta=tesla:cc60,managed -Minfo=accel matrix-acc-tile.c -o matmul
+
+# DRAM
+rm report_Matmul.log
+nvprof --csv --cpu-profiling on \
+    --metrics dram_read_transactions \
+    --print-gpu-summary \
+    --print-openacc-summary \
+    --print-openmp-summary \
+    --print-api-summary \
+    --print-summary \
+    --print-summary-per-gpu \
+    -o report_DRAM.nvprof \
+    --log-file report_DRAM.log ./matmul >> report_Matmul.log
+
+
+# DP
+nvprof --csv --cpu-profiling on \
+    --metrics flop_count_dp \
+    --print-gpu-summary \
+    --print-openacc-summary \
+    --print-openmp-summary \
+    --print-api-summary \
+    --print-summary \
+    --print-summary-per-gpu \
+    -o report_DP.nvprof \
+    --log-file report_DP.log ./matmul >> report_Matmul.log
+
+# Timing vs Power
+# nvprof --csv --system-profiling on --log-file report_Time_Power.log ./matmul >> report_Matmul.log
+
+# Human-readable report
+nvprof --csv --system-profiling on \
+    --devices 0 \
+    --log-file report.log ./matmul >> report_Matmul.log
